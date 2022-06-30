a6a555b62c53392bf0ce883db33ba69cd0297902
CK-Gnap <chanyaporn.kidtee@gmail.com>
2022-07-01 04:42:21 +0700

    Add constants success message

---
+++ constants/success_message.go
@@ -0,0 +1,16 @@
+package constants
+
+var (
+	SuccessCreateCavas    = "canvas created successfully"
+	SuccessGetCanvases    = "get all canvas successfully"
+	SuccessGetCanvas      = "get canvas successfully"
+	SuccessUpdateCanvas   = "canvas updated successfully"
+	SuccessDeleteCanvas   = "canvas deleted successfully"
+	SuccessCreateShape    = "shape created successfully"
+	SuccessGetShapes      = "get all shape successfully"
+	SuccessGetShape       = "get shape successfully"
+	SuccessUpdateShape    = "shape updated successfully"
+	SuccessDeleteShape    = "shape deleted successfully"
+	SuccessTotalArea      = "get total area of shapes successfully"
+	SuccessTotalPerimeter = "get total perimeter of shapes successfully"
+)
